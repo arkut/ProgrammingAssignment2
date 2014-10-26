b1170942f30408753f9b8d90e9bc50977ef62174
Randy Rosso <rrosso@mathematica-mpr.com>
2014-10-26 17:54:37 -0400

    final commit with comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,24 @@
 ## object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+	    # create variable m in parent environment, set to NULL
         m <- NULL
+        # define set function, assigning value of x as input
+        # from makeCacheMatrix function
+        # assign parent env m to NULL so it can be reset
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        # define get function, returning x from makeCacheMatrix
+        # from parent environment
         get <- function() x
+        # define setinv function as inverse function in parent environment
         setinv <- function(solve) m <<- solve
+        # define getinv function, returning m from setinv
         getinv <- function() m
+        # assign functions from makeCacheMatrix to list object to return
+        # this makes functions accessible outside of makeCacheMatrix function
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
@@ -27,13 +37,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        # assign getinv function from x to m
         m <- x$getinv()
+        # if m already defined, use cached value
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+        # assign get function from makeCacheMatrix to data
         data <- x$get()
+        # apply solve function to data, assign result to m
         m <- solve(data, ...)
+        # assign setinv function to m and update m in parent environment
         x$setinv(m)
+        # return m
         m
 }
